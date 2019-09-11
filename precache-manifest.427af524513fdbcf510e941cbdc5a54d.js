15d5c4e4b9a9fe931106df337060262b3c3422a1
josprins <jco.prins@gmail.com>
2019-09-11 15:30:51 +0200

    Updates

---
+++ precache-manifest.427af524513fdbcf510e941cbdc5a54d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1e63746281f86c7edadaaaa328f28f4",
+    "url": "/techdegree-project-11-deploy/index.html"
+  },
+  {
+    "revision": "0ce1f1b45f217244704f",
+    "url": "/techdegree-project-11-deploy/static/css/main.497edbfa.chunk.css"
+  },
+  {
+    "revision": "ed4959ce2d0848e530ed",
+    "url": "/techdegree-project-11-deploy/static/js/2.6ed63092.chunk.js"
+  },
+  {
+    "revision": "0ce1f1b45f217244704f",
+    "url": "/techdegree-project-11-deploy/static/js/main.75770558.chunk.js"
+  },
+  {
+    "revision": "db7c10c6e36576802c08",
+    "url": "/techdegree-project-11-deploy/static/js/runtime~main.06859964.js"
+  }
+]);
